4590c02746d94737d9190912ea53894306039f02
Julie Qiu <julie@golang.org>
2021-09-25 16:56:19 -0400

    publish.sh: check for empty args

--- publish.sh
+++ publish.sh
@@ -1,3 +1,13 @@
+if [ -z "$1" ]
+  then
+    echo "No set specified."
+    exit 1
+fi
+if [ -z "$2" ]
+  then
+    echo "No challenge specified"
+    exit 1
+fi
 u="github.com/julieqiu/cryptopals@"$(go list -m -f {{.Version}} github.com/julieqiu/cryptopals@$(git rev-parse HEAD)) &&
 curl -X POST "https://pkg.go.dev/fetch/$u" &&
 open "https://pkg.go.dev/$u/set$1/c$2"
