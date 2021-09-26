db4ab511ddca155e823a22eff9865ccfe88044eb
Julie Qiu <julie@golang.org>
2021-09-25 20:59:04 -0400

    set1/c5: solution

--- set1/c5/example_test.go
+++ set1/c5/example_test.go
@@ -9,25 +9,25 @@ const (
 	key = "ICE"
 
 	input = `Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal
-Encrypt it, under the key "ICE", using repeating-key XOR.`
+I go crazy when I hear a cymbal`
 )
 
 func Example() {
-	out := decrypt()
-	fmt.Println(bytesToHex(out))
+	out := bytesToHex(decrypt(input))
+	fmt.Println(out)
 	// output:
-	// 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-	// a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+	// 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
 
 }
 
-func decrypt() []byte {
-	out := make([]byte, len(input))
-	for _, k := range []byte(key) {
-		for i := 0; i < len(input); i++ {
-			c := input[i]
-			out[i] = k ^ c
+func decrypt(line string) []byte {
+	j := 0
+	out := make([]byte, len(line))
+	for i := 0; i < len(line); i++ {
+		out[i] = byte(key[j]) ^ byte(line[i])
+		j += 1
+		if j == 3 {
+			j = 0
 		}
 	}
 	return out
