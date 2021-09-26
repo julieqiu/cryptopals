819f9ca0bd49d8112f1c9e5618aa90b7c9231709
Julie Qiu <julie@golang.org>
2021-09-26 09:17:54 -0400

    set1/c6: add hamming distance doc and func

--- set1/c6/helper.go
+++ set1/c6/helper.go
@@ -1,13 +1,20 @@
 package c6
 
+import "fmt"
+
+// HammingDistance reports he minimum number of substitutions required to
+// change one string into the other. It is assumed that input1 and input2 are
+// the same length.
 func HammingDistance(input1, input2 string) int {
-	var d int
+	var distance int
 	for i := 0; i < len(input1); i++ {
+		c1 := fmt.Sprintf("%08b", input1[i])
+		c2 := fmt.Sprintf("%08b", input2[i])
 		for j := 0; j < 8; j++ {
-			if input1[i]&(1<<uint(j)) != input2[i]&(1<<uint(j)) {
-				d++
+			if c1[j] != c2[j] {
+				distance++
 			}
 		}
 	}
-	return d
+	return distance
 }
